ab799431e5377f9b710e0333938a9e784f6e28e1
Kade Fullmer <fullmer24@gmail.com>
2022-09-16 13:58:54 -0600

    Ingredients TABLE created

--- AllSpice/dbSetup.sql
+++ AllSpice/dbSetup.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
-
 SELECT * FROM accounts;
 
 CREATE TABLE IF NOT EXISTS recipes(
@@ -16,18 +15,22 @@ CREATE TABLE IF NOT EXISTS recipes(
   subtitle VARCHAR(255) NOT NULL, 
   category VARCHAR(255) NOT NULL DEFAULT 'random',
   creatorId VARCHAR(255) NOT NULL,
-  Foreign Key (creatorId) REFERENCES accounts (id)
+  FOREIGN KEY (creatorId) REFERENCES accounts (id)
+)default charset utf8 COMMENT '';
+SELECT * FROM recipes;
+
+CREATE TABLE IF NOT EXISTS ingredients(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  quantity INT NOT NULL,
+  recipeId INT NOT NULL,
+  FOREIGN KEY (recipeId) REFERENCES recipes (id)
 )default charset utf8 COMMENT '';
 
-DROP TABLE recipe;
-
-INSERT INTO recipes
-(picture, title, subtitle, category, creatorId)
+INSERT INTO ingredients
+(name, quantity, recipeId)
 VALUES
-('https://images.unsplash.com/photo-1495546968767-f0573cca821e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80', 'cookbook', 'this is a cook book', 'random', '63238c2efe6a9c0c39e53393');
-
-SELECT * FROM recipes r
-JOIN accounts a ON a.id = r.creatorId;
-
-
+('chocolate', 100, 3);
+SELECT * FROM ingredients;
+DELETE FROM ingredients WHERE id = 1;
 
