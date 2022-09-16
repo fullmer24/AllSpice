502a483637715a250077f237eafeaa801edfd478
Kade Fullmer <fullmer24@gmail.com>
2022-09-16 14:46:50 -0600

    Ingredients Model Created

--- AllSpice/Models/Ingredients.cs
+++ AllSpice/Models/Ingredients.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace AllSpice.Models
 {
     public class Ingredients
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+
+        public int RecipeId { get; set; }
     }
 }
