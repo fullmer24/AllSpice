a19cc983458860def615bcb6a51f11e77dcf9928
Kade Fullmer <fullmer24@gmail.com>
2022-09-18 14:25:29 -0600

    spelling

--- AllSpice/Controllers/IngredientsController.cs
+++ AllSpice/Controllers/IngredientsController.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AllSpice.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class IndredientsController : ControllerBase
+    public class IngredientsController : ControllerBase
     {
-        
+
     }
 }
