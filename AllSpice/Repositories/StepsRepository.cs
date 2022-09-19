a4a6cfb6312e00c731451f17d6e0c8a5f8a74e4b
Kade Fullmer <fullmer24@gmail.com>
2022-09-19 16:45:11 -0600

    steps by recipe id with errors

--- AllSpice/Repositories/StepsRepository.cs
+++ AllSpice/Repositories/StepsRepository.cs
@@ -1,12 +1,34 @@
-using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using AllSpice.Models;
+using Dapper;
 
 namespace AllSpice.Repositories
 {
     public class StepsRepository
     {
-        
+        private readonly StepsRepository
+        public StepsRepository(IDbConnection _db)
+        {
+            _db = db;
+        }
+        internal List<Steps> GetStepsByRecipeId(int id)
+        {
+            string sql = @"
+            SELECT 
+            s.*,
+            r.id
+            FROM steps s
+            JOIN recipes r ON r.id = s.recipeId
+            WHERE s.recipeId =@id;
+            ";
+            List<Steps> steps = _db.Query<Steps, Recipe, Steps>(sql, (steps, recipe) =>
+            {
+                steps.recipeId = recipe.id;
+                return steps;
+            }, new { id }).ToList();
+            return steps;
+        }
     }
 }
